32119b1dc5a7c30f28d31fed320c74dc50f165d5
Lisandro Martinez <lisandromartz@gmail.com>
2024-05-09 22:00:11 -0300

    first commit

---
+++ SGE.Aplicacion/Entidades/Expediente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace SGE.Aplicacion;
+
+public class Expediente
+{
+    // Preguntar sobre ID Expediente
+    public int Id { get; }
+    public string? Caratula { get; set; }
+    public DateTime FechaCreacion { get; }
+    public DateTime FechaUltimaModificacion { get; set; }
+    public int UltimaModificacionUserId { get; set; }
+    public EstadoExpediente Estado { get; set; }
+    
+    public Expediente()
+    {
+
+    }
+}
---
+++ SGE.Aplicacion/Entidades/Tramite.cs
@@ -0,0 +1,17 @@
+﻿namespace SGE.Aplicacion;
+
+public class Tramite
+{
+    public int Id { get; }
+    public int ExpedienteId { get; }
+    public EtiquetaTramite Tipo { get; set; }
+    public string? Contenido { get; set; }
+    public DateTime FechaCreacion { get; }
+    public DateTime FechaUltimaModificacion { get; set; }
+    public int UltimaModificacionUserId { get; set; }
+
+    public Tramite()
+    {
+
+    }
+}
