86015cdc17341bc57ca00848a722d91aeffa5d0d
Lisandro Martinez <lisandromartz@gmail.com>
2024-05-11 14:30:38 -0300

    Configurar inicialmente servicio de actualizacion
    -Ahora se asume que FechaHoraUltimaModificacion y
    IdUsuarioUltimaModificacion de Expediente y Tramite se inicializan
    con FechaHoraCreacion y el id del usuario creador respectivamente
    -EspecificacionCambioEstado contiene la lógica de cambio de estado de
    un expediente según la etiqueta que se le pasa a su metodo CambiarEstado
    por parametros
    -ServicioActualizacionEstado recibe por ActualizarEstado el id de un ex-
    pediente pasado por parametros, busca sus tramites, encuentra el ultimo
    de ellos y utiliza EspecificacionCambioEstado.CambiarEstado para actua-
    lizar el estado del expediente.

--- SGE.Aplicacion/Entidades/Tramite.cs
+++ SGE.Aplicacion/Entidades/Tramite.cs
@@ -7,13 +7,13 @@ public class Tramite
     public EtiquetaTramite Etiqueta { get; set; }
     public string Contenido { get; set; }
     public DateTime FechaHoraCreacion { get; set; }
-    public DateTime? FechaHoraUltimaModificacion { get; set; }
-    public int? IdUsuarioUltimaModificacion { get; set; }
+    public DateTime FechaHoraUltimaModificacion { get; set; }
+    public int IdUsuarioUltimaModificacion { get; set; }
 
     public Tramite() { }
 
     public override string ToString()
     {
-        return $"(Id:{Id}) Perteneciente al Expediente:{ExpedienteId}\nTipo:{Etiqueta}\nCreado:{FechaHoraCreacion}, Modificado por ultima vez:{(FechaHoraUltimaModificacion != null ? (FechaHoraUltimaModificacion + " por Usuario #" + IdUsuarioUltimaModificacion) : "sin modificaciones")}\n{Contenido} ";
+        return $"(Id:{Id}) Perteneciente al Expediente:{ExpedienteId}\nTipo:{Etiqueta}\nCreado:{FechaHoraCreacion}, Modificado por ultima vez:{FechaHoraUltimaModificacion} por Usuario #{IdUsuarioUltimaModificacion}\n{Contenido}";
     }
 }
--- SGE.Aplicacion/Servicios/ServicioActualizacionEstado.cs
+++ SGE.Aplicacion/Servicios/ServicioActualizacionEstado.cs
@@ -1,6 +1,23 @@
 ﻿namespace SGE.Aplicacion;
 
-public class ServicioActualizacionEstado
+public class ServicioActualizacionEstado(EspecificacionCambioEstado especificacion, IExpendienteRepositorio repoExpediente, ITramiteRepositorio repoTramite)
 {
-
+    public void ActualizarEstado(int expedienteId)
+    {
+        List<Tramite> tramites = repoTramite.ConsultarPorExpediente(expedienteId);
+        if(tramites.Count() > 0)
+        {
+            Tramite ultimoTramite = tramites[0];
+            foreach (Tramite tramite in tramites)
+            {
+                if(tramite.FechaHoraUltimaModificacion > ultimoTramite.FechaHoraUltimaModificacion)
+                {
+                    ultimoTramite = tramite;
+                }
+            }
+            Expediente expediente = repoExpediente.ConsultarPorID(expedienteId);
+            especificacion.CambiarEstado(expediente, ultimoTramite.Etiqueta);
+            repoExpediente.Modificar(expediente);
+        }
+    }
 }
