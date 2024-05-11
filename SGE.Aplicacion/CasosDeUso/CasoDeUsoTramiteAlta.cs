35c5e856dd7d7ce85e26c524e8b4fd3db5b395a4
Fernando <fernandoramireztolentino@hotmail.com>
2024-05-11 02:29:42 -0300

    Agregando casos de uso de Tramite

--- SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
@@ -1,6 +1,21 @@
 ﻿namespace SGE.Aplicacion;
 
-public class CasoDeUsoTramiteAlta
+public class CasoDeUsoTramiteAlta(ITramiteRepositorio repo, TramiteValidador validador, IServicioAutorizacion servicioAutorizacion)
 {
-
+    public void Ejecutar(Tramite tramite, int idUsuario)
+    {
+        if (idUsuario <= 0)
+        {
+            throw new ArgumentException("El Id del usuario debe ser mayor que 0", nameof(idUsuario));
+        }
+        if (!servicioAutorizacion.PoseeElPermiso(idUsuario, Permiso.TramiteAlta)){
+            throw new AutorizacionException($"El usuario #{idUsuario} no tiene permiso para realizar altas de Tramite");
+        }
+        if (!validador.Validar(tramite, out string mensajeError))
+        {
+            throw new ValidacionException(mensajeError);
+        }
+        tramite.FechaHoraCreacion = DateTime.Now;
+        repo.Agregar(tramite);
+    }
 }
--- SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteModificacion.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteModificacion.cs
@@ -1,6 +1,22 @@
 ﻿namespace SGE.Aplicacion;
 
-public class CasoDeUsoTramiteModificacion
+public class CasoDeUsoTramiteModificacion(ITramiteRepositorio repo, TramiteValidador validador, IServicioAutorizacion servicioAutorizacion)
 {
-
+    public void Ejecutar(Tramite tramite, int idUsuario)
+    {
+        if (idUsuario <= 0)
+        {
+            throw new ArgumentException("El Id del usuario debe ser mayor que 0", nameof(idUsuario));
+        }
+        if (!servicioAutorizacion.PoseeElPermiso(idUsuario, Permiso.TramiteAlta)){
+            throw new AutorizacionException($"El usuario #{idUsuario} no tiene permiso para realizar altas de Tramite");
+        }
+        if (!validador.Validar(tramite, out string mensajeError))
+        {
+            throw new ValidacionException(mensajeError);
+        }
+        tramite.FechaHoraUltimaModificacion = DateTime.Now;
+        repo.Modificar(tramite);
+    }
 }
+
