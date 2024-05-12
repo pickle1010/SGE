87c7cc6631e4b1cfe56d7240f8c111273458c9ae
Fernando <fernandoramireztolentino@hotmail.com>
2024-05-12 16:39:30 -0300

    Modificando toString() de Tramite, ya no va a contener valores nulos

--- SGE.Aplicacion/Entidades/Tramite.cs
+++ SGE.Aplicacion/Entidades/Tramite.cs
@@ -7,8 +7,8 @@ public class Tramite
     public EtiquetaTramite Etiqueta { get; set; }
     public string Contenido { get; set; }
     public DateTime FechaHoraCreacion { get; set; }
-    public DateTime? FechaHoraUltimaModificacion { get; set; }
-    public int? IdUsuarioUltimaModificacion { get; set; }
+    public DateTime FechaHoraUltimaModificacion { get; set; }
+    public int IdUsuarioUltimaModificacion { get; set; }
 
     public Tramite(string contenido) {
         Contenido = contenido;
@@ -16,6 +16,6 @@ public class Tramite
 
     public override string ToString()
     {
-        return $"(Id:{Id}) Perteneciente al Expediente:{ExpedienteId}\nTipo:{Etiqueta}\nCreado:{FechaHoraCreacion}, Modificado por ultima vez:{(FechaHoraUltimaModificacion != null ? (FechaHoraUltimaModificacion + " por Usuario #" + IdUsuarioUltimaModificacion) : "sin modificaciones")}\n{Contenido} ";
+        return $"(Id:{Id}) Perteneciente al Expediente:{ExpedienteId}\nTipo:{Etiqueta}\nCreado:{FechaHoraCreacion}, Modificado por ultima vez:{FechaHoraUltimaModificacion} por Usuario #{IdUsuarioUltimaModificacion}\n{Contenido} ";
     }
 }
