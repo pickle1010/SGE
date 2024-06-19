a48b6d67730b60f28875eb10c71116157fba607d
Lisandro Martinez <lisandromartz@gmail.com>
2024-06-19 01:49:53 -0300

    Agregar nullable a propiedades del usuario

--- SGE.Aplicacion/Entidades/Usuario.cs
+++ SGE.Aplicacion/Entidades/Usuario.cs
@@ -3,10 +3,10 @@
 public class Usuario
 {
     public int Id { get; set; }
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public string Email { get; set; }
-    public string Contraseña { get; set; }
-    public List<Permiso> Permisos { get; set; }
-    public bool EsAdmin { get; set; }
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+    public string? Email { get; set; }
+    public string? Contraseña { get; set; }
+    public List<Permiso>? Permisos { get; set; }
+    public bool EsAdmin { get; set; } = false;
 }
