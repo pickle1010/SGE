dfb3bbaffe2e66e800cc19a3b81487c28756c6ba
Fernando <fernandoramireztolentino@hotmail.com>
2024-06-19 15:01:07 -0300

    Agregando la clase SGESqlite

---
+++ SGE.Repositorios/SGESqlite.cs
@@ -0,0 +1,11 @@
+﻿namespace SGE.Repositorios;
+using SGE.Aplicacion;
+public class SGESqlite 
+{
+    public static void Inicializar(){
+        using var context = new SGEContext();
+        if(context.Database.EnsureCreated()){
+            Console.WriteLine("La base de datos ha sido creada exitosamente");
+        }
+    }
+}
