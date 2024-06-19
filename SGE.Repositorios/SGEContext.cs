41165b0a1e019536eceeec8d97aa2e6542f5ea27
Fernando <fernandoramireztolentino@hotmail.com>
2024-06-19 14:14:20 -0300

    Implementando la clase SGEContext para la configuración inicial de la base de datos

---
+++ SGE.Repositorios/SGEContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGE.Aplicacion;
+namespace SGE.Repositorios;
+
+public class SGEContext : DbContext
+{
+    #nullable disable
+    public DbSet<Expediente> Expedientes { get; set; }
+    public DbSet<Tramite> Tramites { get; set; } 
+    public DbSet<Usuario> Usuarios { get; set; }
+#nullable restore
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("data source=SGE.sqlite");
+    }
+}
