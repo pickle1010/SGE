e45b12dbc720e56a44ec3c1ed44c5175fb2be960
Lisandro Martinez <lisandromartz@gmail.com>
2024-06-19 00:22:11 -0300

    Agregar entidad Usuario

---
+++ SGE.Aplicacion/Entidades/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace SGE.Aplicacion;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Email { get; set; }
+    public string Contraseña { get; set; }
+    public List<Permiso> Permisos { get; set; }
+    public bool EsAdmin { get; set; }
+}
