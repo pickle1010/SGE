6e62e5fcf1696730e12eddc06af1f5e9393e555b
Fernando <fernandoramireztolentino@hotmail.com>
2024-06-19 23:53:57 -0300

    Finalizando la implementación de RepositorioTramite en SGE.Repositorios

--- SGE.Repositorios/RepositorioTramite.cs
+++ SGE.Repositorios/RepositorioTramite.cs
@@ -4,117 +4,75 @@ namespace SGE.Repositorios;
 
 public class RepositorioTramite : ITramiteRepositorio
 {
-    private List<Tramite> Tramites { get; set; }
-    private int ProximoId { get; set; } = 1;
-    private string DireccionTXT { get; set; }
-
-    public RepositorioTramite(string direccionTXT)
+    private SGEContext context; 
+    public RepositorioTramite(SGEContext context)
     {
-        DireccionTXT = direccionTXT;        
-        Tramites = CargarTramites();
-        if(Tramites.Count > 0){
-            ProximoId = Tramites.Last().Id + 1;
-        }
-    }
-
-    private List<Tramite> CargarTramites()
-    {
-        List<Tramite> tramites = new List<Tramite>();
-        if(Path.Exists(DireccionTXT)){
-            using StreamReader sr = new StreamReader(DireccionTXT, true);
-            while(!sr.EndOfStream)
-            {
-                string? linea = sr.ReadLine();
-                if(linea != null && linea.Length > 0){
-                    string[] atributos = linea.Split(','); 
-                    Tramite tramite = new Tramite(atributos[3],int.Parse(atributos[1]));
-                    tramite.Id = int.Parse(atributos[0]);
-                    tramite.Etiqueta = (EtiquetaTramite) Enum.Parse(typeof(EtiquetaTramite), atributos[2]);
-                    tramite.FechaHoraCreacion = DateTime.Parse(atributos[4]);
-                    tramite.FechaHoraUltimaModificacion = DateTime.Parse(atributos[5]);
-                    tramite.IdUsuarioUltimaModificacion = int.Parse(atributos[6]);
-                    tramites.Add(tramite);
-                }
-            }
-        }
-        else {
-            File.Create(DireccionTXT).Close();
-        }
-        return tramites;
-    }
-
-    private string FormatTramite(Tramite t)
-    {
-        return $"{t.Id},{t.ExpedienteId},{t.Etiqueta},{t.Contenido},{t.FechaHoraCreacion},{t.FechaHoraUltimaModificacion},{t.IdUsuarioUltimaModificacion}";
-    }
+        this.context = context;
+    } 
 
     public void Agregar(Tramite tramite)
     {
-        tramite.Id = ProximoId++;
-        Tramites.Add(tramite);
-        using StreamWriter sw = new StreamWriter(DireccionTXT, true);
-        sw.WriteLine(FormatTramite(tramite));
+        context.Add(tramite);
+        context.SaveChanges();
     }
 
     public Tramite Eliminar(int id)
     {
-        int indice = Tramites.FindIndex(t => t.Id == id);
-        if(indice >= 0){
-            Tramite tramite = Tramites[indice];
-            Tramites.RemoveAt(indice);
-            List<string> lineas = File.ReadAllLines(DireccionTXT).ToList(); 
-            lineas.RemoveAt(indice);
-            File.WriteAllLines(DireccionTXT,lineas);
-            return tramite;      
-        }
-        else 
-        {
+        var tramite = context.Tramites.Where(t => t.Id == id).SingleOrDefault();
+        if (tramite == null){
             throw new RepositorioException($"No existe trámite que tenga el id #{id}");
         }
+        context.Remove(tramite);
+        context.SaveChanges();
+        return tramite;
     }
 
     public void Modificar(Tramite tramite)
     {
-        int indice = Tramites.FindIndex(t => t.Id == tramite.Id);
-        if(indice >= 0){
-            tramite.FechaHoraCreacion = Tramites[indice].FechaHoraCreacion;
-            Tramites[indice] = tramite;
-            string[] lineas = File.ReadAllLines(DireccionTXT);
-            lineas[indice] = FormatTramite(tramite);
-            File.WriteAllLines(DireccionTXT, lineas);
-        }
-        else {
+        var tramiteExistente = context.Tramites.Where(t => t.Id == tramite.Id).SingleOrDefault();
+        if(tramiteExistente == null){
             throw new RepositorioException($"No existe trámite que tenga el id #{tramite.Id}");
         }
+        tramiteExistente.Etiqueta = tramite.Etiqueta;
+        tramiteExistente.Contenido = tramite.Contenido;
+        tramiteExistente.FechaHoraUltimaModificacion = tramite.FechaHoraUltimaModificacion;
+        tramiteExistente.IdUsuarioUltimaModificacion = tramite.IdUsuarioUltimaModificacion;
+        context.SaveChanges();
     } 
 
     public List<Tramite> ConsultarPorEtiqueta(EtiquetaTramite etiqueta)
     {
-        List<Tramite> tramitesConEtiqueta = new List<Tramite>();
-        foreach(Tramite tramite in Tramites){
-            if(tramite.Etiqueta == etiqueta){
-                tramitesConEtiqueta.Add(tramite);
-            }
-        }
-        return tramitesConEtiqueta;
+        return context.Tramites.Where(t => t.Etiqueta == etiqueta).ToList();
     }
 
-    public Tramite ConsultarPorId(int id) => Tramites.Find(t => t.Id == id) ?? throw new RepositorioException($"No existe trámite que tenga el id #{id}");
+    public Tramite ConsultarPorId(int id){
+        var tramite = context.Tramites.Where(t => t.Id == id).SingleOrDefault();
+        if(tramite == null){
+            throw new RepositorioException($"No existe trámite que tenga el id #{id}");
+        }
+        return tramite;
+    }
 
     public List<Tramite> ConsultarPorExpediente(int expedienteID){
-        List<Tramite> tramitesDelExpedienteID = new List<Tramite>();
-        foreach(Tramite tramite in Tramites){
-            if(tramite.ExpedienteId == expedienteID){
-                tramitesDelExpedienteID.Add(tramite);
-            }
-        }
-        if(tramitesDelExpedienteID != null)
-        {
-            return tramitesDelExpedienteID;
-        }
-        else 
-        {
+        var tramites = context.Tramites.Where(t => t.ExpedienteId == expedienteID).ToList();
+        if(tramites == null){
             throw new RepositorioException($"No existen trámites asociados al expediente con id #{expedienteID}");
         }
+        return tramites;
+    }
+
+    public void EliminarPorExpediente(int expedienteID){
+        var expediente = context.Expedientes.Where(e => e.Id == expedienteID).SingleOrDefault();
+        if(expediente == null){
+            throw new RepositorioException($"No existe expediente que tenga el id #{expedienteID}");
+        }
+        
+        var tramites = context.Tramites.Where(t => t.ExpedienteId == expedienteID).ToList();
+        if(tramites == null){
+            throw new RepositorioException($"No existen trámites asociados al expediente con id #{expedienteID}");
+        }
+
+        context.Remove(tramites);
+        context.SaveChanges();
     }
 }
