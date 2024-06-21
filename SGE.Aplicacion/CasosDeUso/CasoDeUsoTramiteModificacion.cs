c38f86044d2350fcc59da301619f56d053449311
Lisandro Martinez <lisandromartz@gmail.com>
2024-06-21 15:19:53 -0300

    Modificando estilos y un permiso erroneo en caso de uso

--- SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteModificacion.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteModificacion.cs
@@ -8,8 +8,8 @@ public class CasoDeUsoTramiteModificacion(ITramiteRepositorio repo, TramiteValid
         {
             throw new ArgumentException("El Id del usuario debe ser mayor que 0", nameof(idUsuario));
         }
-        if (!servicioAutorizacion.PoseeElPermiso(idUsuario, Permiso.TramiteAlta)){
-            throw new AutorizacionException($"El usuario #{idUsuario} no tiene permiso para realizar altas de Tramite");
+        if (!servicioAutorizacion.PoseeElPermiso(idUsuario, Permiso.TramiteModificacion)){
+            throw new AutorizacionException($"El usuario #{idUsuario} no tiene permiso para realizar modificaciones de Tramite");
         }
         if (!validador.Validar(tramite, out string mensajeError))
         {
